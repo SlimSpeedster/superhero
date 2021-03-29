3151ebbba2b2222e6266169855a2a602eecd837d
SlimSpeedster <yashdgandhi@outlook.com>
2021-03-29 08:55:20 -0400

    finished

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, box2, box3,box4;
+var box1, box2, box3,box4,box5, box6,box7,box8,box9,box10,box11,box12;
 var hero,monster,rope,ground;
 
 function preload() {
@@ -23,9 +23,17 @@ function setup() {
   monster = new Monster(1100,550,300);
 
   box1 = new Box(600, 100, 70, 70);
+  box5 = new Box(600, 100, 70, 70);
+  box6 = new Box(600, 100, 70, 70);
+  box7 = new Box(600, 100, 70, 70);
+  box8 = new Box(600, 100, 70, 70);
+  box9 = new Box(600, 100, 70, 70);
   box2 = new Box(900, 100, 70, 70);
   box3 = new Box(900, 100, 70, 70);
   box4 = new Box(900, 100, 70, 70);
+  box10 = new Box(900, 100, 70, 70);
+  box12 = new Box(900, 100, 70, 70);
+  box11 = new Box(900, 100, 70, 70);
 
 }
 
@@ -37,9 +45,25 @@ function draw() {
   box2.display();
   box3.display();
   box4.display()
+  box6.display();
+  box5.display(); 
+  box7.display(); 
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+   
+   
 
   hero.display();
   rope.display();
   monster.display();
+
+}
+
+function mouseDragged(){
+
+  Matter.Body.setPosition(hero.body, {x: mouseX, y: mouseY});
 
 }
